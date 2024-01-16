c0fc43f46907cd6f028c70896687d8dae3ec7cc8
Ronit Jai Prakash <128621788+RonitJaiPrakash@users.noreply.github.com>
2024-01-16 12:17:57 +0530

    school content filled

--- src/Components/SchoolComponents/SchoolCarousel.jsx
+++ src/Components/SchoolComponents/SchoolCarousel.jsx
@@ -17,60 +17,56 @@ const SchoolCarousel = () => {
 
   const sections = [
     {
-      title: 'Visual Collaboration',
+      title: 'Communication',
       content: (
-        <div className="flex">
-          <div className="w-1/2 p-4">
-            <h2 className="text-2xl font-bold mb-4">Enhance Collaboration</h2>
-            <p>Foster visual collaboration among teachers, students, and parents using intuitive tools and features.</p>
-          </div>
-          <div className="w-1/2 p-4">
-            <h2 className="text-2xl font-bold mb-4">Streamlined Communication</h2>
+        <div className="flex flex-wrap">
+          <div className="w-full lg:w-1/2 p-4">
+            <img src="https://static-cse.canva.com/blob/1117535/School_leaders_administrators2x.33984be3.avif" alt="" srcset="" width={"100%"}/>
+             </div>
+          <div className="w-full lg:w-1/2 p-4 flex flex-col justify-center">
+            <h2 className="text-2xl font-bold mb-4 cera-med ">Communication</h2>
             <p>Streamline communication with real-time updates, making it easy for everyone to stay informed.</p>
           </div>
         </div>
       ),
     },
     {
-      title: 'Efficient Workflows',
+      title: 'Leanring',
       content: (
-        <div className="flex">
-          <div className="w-1/2 p-4">
-            <h2 className="text-2xl font-bold mb-4">Boost Efficiency</h2>
-            <p>Improve workflows and task management, ensuring smooth and efficient operations for the entire school community.</p>
-          </div>
-          <div className="w-1/2 p-4">
-            <h2 className="text-2xl font-bold mb-4">Task Assignments</h2>
+        <div className="flex flex-wrap">
+           <div className="w-full lg:w-1/2 p-4">
+            <img src="https://static-cse.canva.com/blob/1117536/Teachers2x.d3515424.avif" alt="" srcset="" width={"100%"}/>
+             </div>
+          <div className="w-full lg:w-1/2 p-4 flex flex-col justify-center">
+            <h2 className="text-2xl font-bold mb-4 cera-med " >Leanring</h2>
             <p>Assign tasks, projects, and assignments with ease, promoting a structured learning environment.</p>
           </div>
         </div>
       ),
     },
     {
-      title: 'Secure Data Handling',
+      title: 'Data',
       content: (
-        <div className="flex">
-          <div className="w-1/2 p-4">
-            <h2 className="text-2xl font-bold mb-4">Data Security</h2>
-            <p>Ensure the safety of sensitive information with Techlearns' robust data security measures, complying with industry standards.</p>
-          </div>
-          <div className="w-1/2 p-4">
-            <h2 className="text-2xl font-bold mb-4">Privacy Controls</h2>
+        <div className="flex flex-wrap">
+          <div className="w-full lg:w-1/2 p-4">
+            <img src="https://static-cse.canva.com/blob/1117533/Students2x.ab46e5e5.avif" alt="" srcset="" width={"100%"}/>
+             </div>
+          <div className="w-full lg:w-1/2 p-4 flex flex-col justify-center">
+            <h2 className="text-2xl font-bold mb-4 cera-med ">Data</h2>
             <p>Grant administrators control over privacy settings, allowing them to manage data access and protect student information.</p>
           </div>
         </div>
       ),
     },
     {
-      title: 'Customizable Learning Paths',
+      title: 'Workflows',
       content: (
-        <div className="flex">
-          <div className="w-1/2 p-4">
-            <h2 className="text-2xl font-bold mb-4">Tailored Learning</h2>
-            <p>Offer personalized learning experiences by creating customizable learning paths for students based on their individual needs and pace.</p>
-          </div>
-          <div className="w-1/2 p-4">
-            <h2 className="text-2xl font-bold mb-4">Adaptive Assessments</h2>
+        <div className="flex flex-wrap ">
+          <div className="w-full lg:w-1/2 p-4">
+            <img src="https://static-cse.canva.com/blob/1117527/Marketing_communications2x.c2492b10.avif" alt="" srcset="" width={"100%"}/>
+             </div>
+          <div className="w-full lg:w-1/2 p-4 flex flex-col justify-center">
+            <h2 className="text-2xl font-bold mb-4 cera-med">Workflows</h2>
             <p>Implement adaptive assessments that adjust difficulty based on students' performance, ensuring a challenging yet supportive educational journey.</p>
           </div>
         </div>
@@ -85,9 +81,9 @@ const SchoolCarousel = () => {
   };
 
   return (
-    <div className="w-4/5 m-auto text-center">
+    <div className="w-4/5 m-auto">
       <div className="text-center">
-        <p className="section-title">
+        <p className="section-title cera-med">
           Bring all the benefits of Techlearns to your school community
         </p>
       </div>
@@ -97,10 +93,10 @@ const SchoolCarousel = () => {
             <button
               key={index}
               onClick={() => handleButtonClick(index)}
-              className={`px-4 py-2 text-white ${activeSection === index
-                ? 'bg-blue-500'
-                : 'bg-gray-500 hover:bg-gray-600'
-                } rounded focus:outline-none focus:ring focus:border-blue-300`}
+              className={`px-4 py-2 text-blue-900 text-lg cera-med ${activeSection === index
+                ? 'border-b-2 border-blue-900 bg-transparent'
+                : 'bg-transparent'
+                } rounded focus:outline-none`}
             >
               {section.title}
             </button>
@@ -108,9 +104,9 @@ const SchoolCarousel = () => {
         </div>
         <Slider ref={sliderRef} {...settings}>
           {sections.map((section, index) => (
-            <div key={index} className="outline-none">
-              <h2 className="text-2xl font-bold mb-4">{section.title}</h2>
-              <p>{section.content}</p>
+            <div key={index} className="outline-none flex flex-col justify-center">
+              {/* <h2 className="text-2xl font-bold mb-4">{section.title}</h2> */}
+              <p className='flex flex-col justify-center'>{section.content}</p>
             </div>
           ))}
         </Slider>
